668978232a0179a27ea5fec15b6c8b8ec2474fde
dennis.markmann <dennismarkmann@hotmail.de>
2013-07-10 13:25:18 +0200

    Additional commit.

---
+++ MyLibraries/src/dennis/markmann/MyLibraries/Main.java
@@ -0,0 +1,14 @@
+package dennis.markmann.MyLibraries;
+
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
